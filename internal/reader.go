8dd81b1d14a3b1d4937f1a0ebb9374049938de24
BubbleFish <daforsastudia@gmail.com>
2024-08-21 10:38:43 +0300

    feat(internal): Add Reader.ReadRunes

--- internal/reader.go
+++ internal/reader.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"encoding/binary"
 	"fmt"
 	"io"
 )
@@ -19,6 +20,18 @@ func (reader *Reader) Read(field string, data []byte) error {
 	_, err := io.ReadFull(reader.reader, data)
 	if err != nil {
 		return fmt.Errorf("Error reading: %q: %w", field, err)
+	}
+
+	return nil
+}
+
+func (reader *Reader) ReadRunes(field string, data []rune) error {
+	var char rune
+	for i := range data {
+		if err := binary.Read(reader.reader, binary.LittleEndian, &char); err != nil {
+			return fmt.Errorf("Error reading: %q: %w", field, err)
+		}
+		data[i] = char
 	}
 
 	return nil
