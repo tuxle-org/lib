268f818601391acf345cd86dd55d03f90b787173
BubbleFish <daforsastudia@gmail.com>
2024-10-01 15:37:26 +0300

    refactor: Move to new bbfh-dev/go-orm format

--- tuxle/entities/server.go
+++ tuxle/entities/server.go
@@ -1,12 +1,12 @@
-package tables
+package entities
 
 type Server struct {
-	ServerId    int    `db:"server_id"   create:"INTEGER PRIMARY KEY"`
+	ServerId    int64  `db:"server_id"   create:"INTEGER PRIMARY KEY"`
 	Name        string `db:"name"        create:"TEXT NOT NULL DEFAULT ''"`
 	Description string `db:"description" create:"TEXT NOT NULL DEFAULT ''"`
 	IconURI     string `db:"icon_uri"    create:"TEXT NOT NULL DEFAULT ''"`
 }
 
-func (table Server) Table() string {
+func (table Server) SQL() string {
 	return "server"
 }
